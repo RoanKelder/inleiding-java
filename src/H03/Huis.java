e09d3f7c062957f18147a5414ed0aafce4103c74
RoanKelder <rkelder@roc-dev.com>
2019-09-04 18:06:12 +0200

    Vormen

--- src/H03/Huis.java
+++ src/H03/Huis.java
@@ -13,12 +13,12 @@ public class Huis extends Applet {
 
     @Override
     public void paint(Graphics g) {
-        setBackground(Color.GREEN);
+        setBackground(Color.green);
         g.setColor(Color.black);
         g.drawRect(40,100,200,200);
-        g.drawRect(200,250,20,50);
-        g.drawLine(40, 100,150,0);
-        g.drawLine(240,100,150,0);
+        g.drawRect(175,250,35,50);
+        g.drawLine(40, 100,140,0);
+        g.drawLine(240,100,140,0);
         g.drawRect(60,200,50,70);
     }
 }
