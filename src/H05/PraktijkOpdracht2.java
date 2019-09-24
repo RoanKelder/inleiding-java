5445a50f0740500063b23f5d57bff228250fed4c
RoanKelder <rkelder@roc-dev.com>
2019-09-24 09:15:04 +0200

    Vormen

--- src/H05/PraktijkOpdracht2.java
+++ src/H05/PraktijkOpdracht2.java
@@ -11,22 +11,21 @@ public class PraktijkOpdracht2 extends Applet {
 
     @Override
     public void paint(Graphics g) {
-        g.drawLine(20,20,120,20);
-        g.drawRect(20,40,100, 50);
-        g.drawRoundRect(20,110,100,50,10,10);
+        g.drawLine(20, 20, 120, 20);
+        g.drawRect(20, 40, 100, 50);
+        g.drawRoundRect(20, 110, 100, 50, 10, 10);
         g.setColor(Color.green);
-        g.fillRect(140,40,100,50);
+        g.fillRect(140, 40, 100, 50);
         g.setColor(Color.black);
-        g.drawArc(140,40,100,50,45,360);
+        g.drawArc(140, 40, 100, 50, 45, 360);
         g.setColor(Color.green);
-        g.fillArc(140, 110, 100,50,45,360);
+        g.fillArc(140, 110, 100, 50, 45, 360);
         g.setColor(Color.black);
-        g.drawArc(260,40,100,50,90,360);
+        g.drawArc(260, 40, 100, 50, 90, 360);
         g.setColor(Color.green);
-        g.fillArc(260,40,100,50,0,45);
+        g.fillArc(260, 40, 100, 50, 0, 45);
         g.setColor(Color.black);
-        g.drawArc(280,110, 50,50,45,360);
-
+        g.drawArc(280, 110, 50, 50, 45, 360);
 
 
     }
